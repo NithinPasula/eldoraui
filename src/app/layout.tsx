52cc4bef018757815227e266d0b7fc253b58c04d
NithinPasula <nithin.pasula@gmail.com>
2024-10-15 20:14:53 +0530

    modified layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import "./globals.css";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Saas Template - EldoraUI",
-  description: "Template for saas applications with dark theme",
+  title: "Dark Saas Template - EldoraUI",
+  description:
+    "The EldoraUI SaaS Template is designed specifically for SaaS applications, offering a sleek and modern interface with a built-in dark theme. This template provides an efficient starting point for developing scalable and customizable software solutions, integrating essential features to enhance the user experience. With a clean design and responsive layout, it is ideal for building professional applications that focus on both functionality and aesthetic appeal.",
 };
 
 export default function RootLayout({
